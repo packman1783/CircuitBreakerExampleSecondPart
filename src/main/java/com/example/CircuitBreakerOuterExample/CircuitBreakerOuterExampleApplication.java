1f387f92595b9c5549240077cd072505b52ea889
Denis Saplitsyn <d.saplitsyn@mail.astondevs.ru>
2026-04-03 11:27:19 +0300

    создан спрингом

---
+++ src/main/java/com/example/CircuitBreakerOuterExample/CircuitBreakerOuterExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.CircuitBreakerOuterExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircuitBreakerOuterExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CircuitBreakerOuterExampleApplication.class, args);
+	}
+
+}
