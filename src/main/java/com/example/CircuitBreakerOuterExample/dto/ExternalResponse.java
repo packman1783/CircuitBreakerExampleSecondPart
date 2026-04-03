d4bd3ad2c9a8fab42d1da19199c46bb828951364
Denis Saplitsyn <d.saplitsyn@mail.astondevs.ru>
2026-04-03 11:27:59 +0300

    создан ExternalResponse

---
+++ src/main/java/com/example/CircuitBreakerOuterExample/dto/ExternalResponse.java
@@ -0,0 +1,13 @@
+package com.example.CircuitBreakerOuterExample.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExternalResponse {
+
+    private String message;
+}
