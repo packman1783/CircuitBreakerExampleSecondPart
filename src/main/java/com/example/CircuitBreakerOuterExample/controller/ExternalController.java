1553394add2f91950f573b568d97590c55cb6794
Denis Saplitsyn <d.saplitsyn@mail.astondevs.ru>
2026-04-03 11:27:48 +0300

    создан ExternalController

---
+++ src/main/java/com/example/CircuitBreakerOuterExample/controller/ExternalController.java
@@ -0,0 +1,22 @@
+package com.example.CircuitBreakerOuterExample.controller;
+
+import com.example.CircuitBreakerOuterExample.dto.ExternalResponse;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ExternalController {
+
+    @GetMapping("/data")
+    public ExternalResponse getData() {
+        return new ExternalResponse("Внешний сервис (8081) доступен и работает корректно");
+    }
+
+    @GetMapping("/fail")
+    public ExternalResponse fail() {
+        throw new RuntimeException("Сервис упал");
+    }
+}
